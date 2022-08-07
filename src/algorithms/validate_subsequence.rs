161a376e91335bae463657efc1cefbf9086cf128
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-08-07 17:45:51 +0430

    validate subsequence 3

--- src/algorithms/validate_subsequence.rs
+++ src/algorithms/validate_subsequence.rs
@@ -35,6 +35,17 @@ pub fn validate_subsequence_2(arr: &[i32], sub: &[i32]) -> bool {
     return i_sub == len_sub;
 }
 
+pub fn validate_subsequence_3(arr: &[i32], sub: &[i32]) -> bool {
+    match (arr.len(), sub.len()) {
+        (_, 0) => true,
+        (0, _) => false,
+        (_, _) => match arr[0] == sub[0] {
+            true => validate_subsequence_3(&arr[1..], &sub[1..]),
+            false => validate_subsequence_3(&arr[1..], &sub),
+        },
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -69,5 +80,21 @@ mod tests {
         let sub = vec![1, 6, -9, 35, 0, 4, -6, 1];
 
         assert!(!validate_subsequence_2(&arr, &sub));
+    }
+
+    #[test]
+    fn validate_subsequence_3_should_return_true_if_second_arr_is_sub_of_first() {
+        let arr = vec![1, 5, 9, 7, 3, 4, 6, -9, 1, 0, 7, 4, -6, 1];
+        let sub = vec![1, 6, -9, 0, 4, -6, 1];
+
+        assert!(validate_subsequence_3(&arr, &sub));
+    }
+
+    #[test]
+    fn validate_subsequence_3_should_return_false_if_second_arr_is_not_sub_of_first() {
+        let arr = vec![1, 5, 9, 7, 3, 4, 6, -9, 1, 0, 7, 4, -6, 1];
+        let sub = vec![1, 6, -9, 35, 0, 4, -6, 1];
+
+        assert!(!validate_subsequence_3(&arr, &sub));
     }
 }
