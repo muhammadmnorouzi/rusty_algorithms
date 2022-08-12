7d0e10cda2e937ff0a1313d68efaf44182d72605
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2022-08-12 12:32:55 +0430

    mod corrected

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,4 +1,4 @@
-pub use find_closest_value_in_bst::find_closest_value_in_bst_1;
+pub use find_closest_value_in_bst::{find_closest_value_in_bst_1, find_closest_value_in_bst_2};
 pub use two_sum::{two_sum_1, two_sum_2, two_sum_3};
 pub use validate_subsequence::{
     validate_subsequence_1, validate_subsequence_2, validate_subsequence_3,
